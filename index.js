1f81121739c19ac6ba8dad55aeb6387308fc7103
Sandeep Sharma <sandeepsharma@naraci.com>
2024-09-21 01:59:29 +0530

    test

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ let cursor = document.getElementsByClassName('blob');
 if (cursor) {
   document.addEventListener('mousemove', function(e) {
     console.log("Mouse moving", cursor);
-    var x = e.clientX;
-    var y = e.clientY;
+    let x = e.clientX;
+    let y = e.clientY;
     cursor[0].style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
   });
 } else {
